d7bf344e1782890a65818a4ba1bb767db74e2358
levmorgot <levmorgot@gmail.com>
2021-11-19 17:19:05 +0300

    fix not load bug

--- lib/services/filial_api_provider.dart
+++ lib/services/filial_api_provider.dart
@@ -5,13 +5,19 @@ import 'package:http/http.dart' as http;
 
 class FilialProvider {
   List<Filial> _allFilials = [];
+
   Future<List<Filial>> getFilial() async {
-    final response = await http.get(Uri.parse('https://registratura.volganet.ru/filial'));
+    final response =
+        await http.get(Uri.parse('https://registratura.volganet.ru/filial'));
     if (response.statusCode == 200) {
       final List<dynamic> filialJson = json.decode(response.body)['data'];
-      filialJson.map((json) {
-        _allFilials.add(Filial.fromJson(json));
-      });
+      for (final json in filialJson) {
+        try {
+          _allFilials.add(Filial.fromJson(json));
+        } catch (_) {
+          print('Error fetching filial ${json}');
+        }
+      }
       return _allFilials;
     } else {
       throw Exception('Error fetching filials');
