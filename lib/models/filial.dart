d82b8a47f353d284b8d3b72e5192ea8df3600eae
levmorgot <levmorgot@gmail.com>
2021-11-19 19:25:38 +0300

    Испольование cashId

--- lib/models/filial.dart
+++ lib/models/filial.dart
@@ -1,14 +1,16 @@
 class Filial {
   int id;
+  int cashId;
   String name;
   String address;
   String phone;
 
-  Filial({this.id: 0, this.name: '', this.address: '', this.phone: ''});
+  Filial({this.id: 0, this.cashId: 0, this.name: '', this.address: '', this.phone: ''});
 
   factory Filial.fromJson(Map<String, dynamic> json) {
     return Filial(
         id: json['id'],
+        cashId: json['cashId'],
         name: json['name'],
         address: json['address'],
         phone: json['phone'],
